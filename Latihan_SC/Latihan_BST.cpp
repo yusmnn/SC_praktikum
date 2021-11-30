d6cf08a6813cfda5a49528fd76be9b0eccc8c51d
yusmnn <yusmanqq@gmail.com>
2021-12-01 01:37:23 +0800

    update

--- Latihan_SC/Latihan_BST.cpp
+++ Latihan_SC/Latihan_BST.cpp
@@ -1,14 +1,37 @@
-#include <iostream>
-#include <queue>
+#include <bits/stdc++.h>
+#include <stdlib.h>
+#include <conio.h>
+
 using namespace std;
 
-struct Node{
-	int data;
-	Node *left;
-	Node *right;
+struct Node{ 
+  int data;
+  Node *left;
+  Node *right;
 };
 
-Node *root=NULL, *n=NULL, *temp=NULL;
+Node *root = NULL, *n = NULL, *temp = NULL;
+
+int getOption(){
+	int input;
+	system("cls");
+	cout << "Menu: " << endl;
+	cout << "1. Insert Node" << endl;
+	cout << "2. Pencarian data" << endl;
+	cout << "3. Hapus Node" << endl;
+	cout << "4. Binary tree traversal" << endl;
+	cout << "5. Keluar" << endl;
+	cout << "Masukkan Pilihan 1 - 5 : ";
+	cin >> input;
+	return input;
+}
+
+bool isEmpty(){
+	if(root == NULL)
+		return true;
+	else
+	return false;
+}
 
 Node *createNode(int data){
 	n = new Node;
@@ -29,48 +52,46 @@ void insert(Node *&root, int data){
 
 void levelOrder(){
 	if (root==NULL) return;
-	
+
 	queue<Node*> q;
 	q.push(root);
+
 	while(!q.empty()){
-		cout << q.front()->data << " ";
+	cout << q.front()->data << " ";
 		if(q.front()->left != NULL)
 			q.push(q.front()->left);
 		if(q.front()->right != NULL)
 			q.push(q.front()->right);
-		q.pop();
+			q.pop();
 	}
 }
 
 void preOrder(Node *root){
 	if (root==NULL) return;
-	
 	cout << root->data << " ";
 	preOrder(root->left);
 	preOrder(root->right);
 }
 
-void inOrder(Node *root){
+void inOrder_asc(Node *root){
 	if (root==NULL) return;
-	
-	inOrder(root->left);
+	inOrder_asc(root->left);
 	cout << root->data << " ";
-	inOrder(root->right);
+	inOrder_asc(root->right);
 }
 
-void inOrderDesc(Node *root){
+void inOrder_Desc(Node *root){
 	if (root==NULL) return;
-	inOrderDesc(root->right);
+	inOrder_Desc(root->right);
 	cout << root->data << " ";
-	inOrderDesc(root->left);
+	inOrder_Desc(root->left);
 }
 
 void postOrder(Node *root){
 	if (root==NULL) return;
-	
-	postOrder(root->left);
-	postOrder(root->right);
-	cout << root->data << " ";
+		postOrder(root->left);
+		postOrder(root->right);
+		cout << root->data << " ";
 }
 
 bool cari(Node *root, int data){
@@ -100,154 +121,165 @@ void hapus(Node *&root, int data){
 	else if(data > root->data)
 		return hapus(root->right, data);
 	else{
-		// kasus I
-		if (root->left == NULL && root->right == NULL){
-			delete(root);
-			root=NULL;
+	// kasus I
+	if (root->left == NULL && root->right == NULL){
+		delete(root);
+		root=NULL;
 		}
 		// kasus II
-		else if (root->left == NULL){
-			temp = root;
-			root = root->right;
-			delete(temp);
-			temp=NULL;
+	else if (root->left == NULL){
+		temp = root;
+		root = root->right;
+		delete(temp);
+		temp=NULL;
 		}
 		else if (root->right == NULL){
-			temp = root;
-			root = root->left;
-			delete(temp);
-			temp=NULL;
-		}
-		// kasus III
-		else{
-			temp = cariMin(root->right);
-			root->data = temp->data;
-			hapus(root->right, data);
+		temp = root;
+		root = root->left;
+		delete(temp);
+		temp=NULL;
+	}
+	// kasus III
+	else{
+		temp = cariMin(root->right);
+		root->data = temp->data;
+		hapus(root->right, root->data);
 		}
 	}
-}
-
-bool isEmpty(){
-	if (root==NULL){
-		return true;
-} else {
-	return false;
-}
 }
 
 int main(){
-	int pilihan,n,num,temp;
-	do{
-	system("cls");
-	cout<<"menu pilihan : "<<endl;
-	cout<<"1. Masukkan Node"<<endl;
-	cout<<"2. Tampilkan dalam bentuk levelOrder"<<endl;
-	cout<<"3. Tampilkan dalam bentuk preOrder"<<endl;
-	cout<<"4. Tampilkan dalam bentuk inOrder"<<endl;
-	cout<<"5. Tampilkan dalam bentuk inOrder desc"<<endl;
-	cout<<"6. Tampilkan dalam bentuk postOrder"<<endl;
-	cout<<"7. Cari NOde"<<endl;
-	cout<<"8. Hapus Node"<<endl;
-	cout<<"9. exit"<<endl;
-	cout<<"MASUKKAN PILIHAN [1....9] : ";
-	cin>>pilihan;
+	int pilihan, i, temp;
 	
-	system("cls");
-	switch(pilihan)
-	
+	do
 	{
-		case 1: {
-			
-			cout<<"masukkan node baru"<<endl;
-			cout<<"=============="<<endl;
-			cout<<"masukkan nilai"<<endl;
-			cin>>n;
-			insert(root,n);
-			cout<<"berhasil "<<endl;
-
-			break;
-		}
-		case 2: {
-			if(isEmpty()){
-				cout<<"node belum dibuat"<<endl;
-			} else {
-				cout<<"menampilkan dalam bentuk levelorder"<<endl;
-				cout<<"==================="<<endl;
-				levelOrder();
-				cout<<"\n";
-				cout<<endl;
-			}
-			break;
-		}
-		case 3: {
-			if(isEmpty()){
-				cout<<"node belum dibuat"<<endl;
-			} else {
-				cout<<"menampilkan dalam bentuk preorder"<<endl;
-				cout<<"==================="<<endl;
-				preOrder(root);
-				cout<<endl;
-			}
-			break;
-		}
-		case 4: {
-			if(isEmpty()){
-				cout<<"node belum dibuat"<<endl;
-			} else {
-				cout<<"menampilkan dalam bentuk inorder"<<endl;
-				cout<<"==================="<<endl;
-				inOrder(root);
-				cout<<endl;
-			}
-			break;
-		}
-		case 5: {
-			if(isEmpty()){
-				cout<<"node belum dibuat"<<endl;
-			} else {
-				cout<<"menampilkan dalam bentuk inorder  (desc)"<<endl;
-				cout<<"==================="<<endl;
-				inOrderDesc(root);
-				cout<<endl;
-			break;
-		}
-		case 6: {
-			if(isEmpty()){
-				cout<<"node belum dibuat"<<endl;
-			} else {
-				cout<<"menampilkan dalam bentuk postorder"<<endl;
-				cout<<"==================="<<endl;
-				postOrder(root);
-				cout<<endl;
-			break;
-		}
-		case 7: {
-			if(isEmpty()){
-				cout<<"node belum dibuat"<<endl;
-			} else {
-				cout<<"masukkan nilai node yang ingin dicari"<<endl;
-				cout<<"==================="<<endl;
-				cin>>n;
-				cari(root,n);
-				cout<<"node dengan nilai : "<<n<<" berhasil ditemukan"<<endl;
-			break;
-		}
-		case 8: {
-			if(isEmpty()){
-				cout<<"node belum dibuat"<<endl;
-			} else {
-				cout<<"masukkan nilai node yang akan dihapus"<<endl;
-				cout<<"==================="<<endl;
-				cin>>n;
-				hapus(root,n);
-				cout<<"node : "<<n<<" berhasi di hapus "<<endl;
-			break;
-		}
-	}
-	}
-	}	
-	}
-	} system("pause");
-	} while(pilihan>=1 && pilihan<=8);
+		pilihan = getOption();
+		
+		switch(pilihan)
+		{
+			case 1:
+				system("cls");
+				cout << "masukkan data: ";
+				cin >> i;
+				insert(root,i);
+				cout << "Data " << i << " berhasil dimasukkan ";
+				break;
+			case 2:
+				system("cls");
+				if(isEmpty()){
+					cout << "Tree Masih Kosong ";
+				}
+				else{
+					cout << "Masukkan data yang akan dicari:";
+					cin >> temp;
+					if(temp == root->data){// parameter masih salah
+						cari(root,temp);				
+						cout << "data " << temp << " berhasil di temukan";				
+					}
+					else{
+						cout << "data " << temp << " tidak ditemukan ";
+					}					
+				}
+				break;
+			case 3:
+				system("cls");
+				if(isEmpty()){
+					cout << "Tree Masih Kosong ";
+				}
+				else{
+					cout << "Masukkan data yang akan dihapus:";
+					cin >> temp;
+					if(temp == root->data){//parameter masih salah	
+						hapus(root,temp);		
+						cout << "data " << temp << " berhasil dihapus";				
+					}
+					else{
+						cout << "data " << temp << " tidak ditemukan ";
+					}					
+				}
+				break;
+			case 4:
+				system("cls");
+				if(isEmpty()){
+					cout << "Tree masih Kosong..";
+				}
+				else{
+					cout << "Binary Tree Traversal" << endl;
+					cout << "====================" << endl;
+					cout << "1. Level order" << endl;
+					cout << "2. Preorder" << endl;
+					cout << "3. Inorder (ASC)" << endl;
+					cout << "4. Inorder (DESC)" << endl;
+					cout << "5. Postorder" << endl;
+					cout << "6. Batal" << endl;
+					cout << "Masukkan pilihan [1..6] : ";
+					cin >> pilihan;
+					system("cls");
+					if(pilihan == 1){
+						cout << "Binary Tree Traversal" << endl;
+						cout << "==================" << endl;
+						cout << "Level order: ";
+						levelOrder();
+					}
+					else if(pilihan == 2){
+						cout << "Binary Tree Traversal" << endl;
+						cout << "==================" << endl;
+						cout << "PreOrder : ";
+						preOrder(root);
+					}
+					else if(pilihan == 3){
+						cout << "Binary Tree Traversal" << endl;
+						cout << "==================" << endl;
+						cout << "InOrder (ASC) : ";
+						inOrder_asc(root);
+					}				
+					else if(pilihan == 4){
+						cout << "Binary Tree Traversal" << endl;
+						cout << "==================" << endl;
+						cout << "InOrder (DESC) : ";
+						inOrder_Desc(root);
+					}
+					else if(pilihan == 5){
+						cout << "Binary Tree Traversal" << endl;
+						cout << "==================" << endl;
+						cout << "PostOrder : ";
+						postOrder(root);
+					}
+					else if(pilihan == 6){
+					return main();
+					}
+					else{
+						cout << "Pilihan tidak ditemukan ";
+						getch();
+						return main();
+					}
+				}				
+				break;
+			case 5:
+				exit(0);
+			default:
+				cout << "Pilihan tidak ditemukan";
+				getch();
+				return main();		
+		}getch();	
+	}while(pilihan >= 1 && pilihan <= 5);
+  return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
